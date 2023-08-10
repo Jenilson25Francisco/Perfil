01f2168fbd4d0f22805516bdd7e9297fc45ee354
Jenilson Viwaldo Francisco <franciscoviwaldo@gmail.com>
2023-08-10 18:39:23 +0100

    finishing the project

--- script.js
+++ script.js
@@ -5,8 +5,13 @@ function toggleMode() {
   const img = document.querySelector("#profile img")
 
   if (html.classList.contains("light")) {
-    img.setAttribute("src", "./assets/avatar-light.png")
+    img.setAttribute("src", "./assets/avatar-light2.png")
+    img.setAttribute("alt", "fotografia de jenilson francisco tirando uma selfie e tem uma camisa preta")
   } else {
     img.setAttribute("src", "./assets/avatar.png")
+    img.setAttribute(
+      "alt",
+      "fotografia de Jenilson Francisco tirando uma selfie"
+    )
   }
 }
